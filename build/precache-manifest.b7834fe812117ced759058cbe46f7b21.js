e4fbef8f400ab6d147a70eb82c4ac6dee701d404
Viram Shah <32411892+viram-ms@users.noreply.github.com>
2019-07-31 22:02:49 +0530

    build folder

---
+++ build/precache-manifest.b7834fe812117ced759058cbe46f7b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a599682035708dd20fe",
+    "url": "/Attendance-front/static/js/runtime~main.7a599682.js"
+  },
+  {
+    "revision": "959f1d9826b6d793a5f7",
+    "url": "/Attendance-front/static/js/main.959f1d98.chunk.js"
+  },
+  {
+    "revision": "873f720c14b3bc2cb393",
+    "url": "/Attendance-front/static/js/1.873f720c.chunk.js"
+  },
+  {
+    "revision": "959f1d9826b6d793a5f7",
+    "url": "/Attendance-front/static/css/main.a83dd05b.chunk.css"
+  },
+  {
+    "revision": "873f720c14b3bc2cb393",
+    "url": "/Attendance-front/static/css/1.03529571.chunk.css"
+  },
+  {
+    "revision": "67c5dee308f77818cfab94b08506feb1",
+    "url": "/Attendance-front/index.html"
+  }
+];
